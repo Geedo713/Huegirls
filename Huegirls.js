be3fa1a3ceb5bcfb15a9b4e6bab969bbdce02246
Geedo Boubakar Ibn Jalloh (M. Clark) <88060042+Geedo713@users.noreply.github.com>
2022-04-20 17:47:17 -0500

    Update Huegirls.js

--- Huegirls.js
+++ Huegirls.js
@@ -1,7 +1,27 @@
-const mysteryText = document.getElementsByClassName("main-div")[1];
+const mysteryText = document.getElementsByTagName('h1')[0];
+const headerBackGround = document.getElementsByTagName('header')[0];
 
 function textInjector() {
     mysteryText.innerText = "What is your skin type?";
 }
 
-document.addEventListener('mousemove', textInjector, false);
+let opacity = 0;
+        let intervalID = 0;
+        window.onload = fadeIn;
+  
+        function fadeIn() {
+            setInterval(show, 100);
+        }
+  
+        function show() {
+            opacity = Number(window.getComputedStyle(headerBackGround)
+                             .getPropertyValue("opacity"));
+            if (opacity < 1) {
+                opacity = opacity + 0.1;
+                headerBackGround.style.opacity = opacity;
+            } else {
+                clearInterval(intervalID);
+            }
+        }
+
+window.setTimeout(textInjector, 2500);
